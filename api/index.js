e479dc0a5eae7f9e09045e05cac43db4e25d8476
grayrey28-dev <grayrey28@gmail.com>
2026-03-31 16:11:01 -0500

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = (req, res) => {
+  // Replace the URL below with the site you want to unblock
+  const target = "https://www.tiktok.com"; 
+
+  const proxy = createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: { '^/api': '' }, // Cleans the URL path
+    onProxyRes: (proxyRes) => {
+      // Deletes security headers that prevent "framing" or "mirroring"
+      delete proxyRes.headers['content-security-policy'];
+      delete proxyRes.headers['x-frame-options'];
+    }
+  });
+
+  return proxy(req, res);
+};
